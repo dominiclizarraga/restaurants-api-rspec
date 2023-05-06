065e0cbe38b52d7440bb570505e6cb4ba211c002
dominic lizarraga <dominiclizarraga@hotmail.com>
2023-05-05 22:15:13 -0700

    add test for nil

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,3 @@
 class Restaurant < ApplicationRecord
   validates :name, :address, presence: true
-
 end
--- spec/requests/get_restaurants_spec.rb
+++ spec/requests/get_restaurants_spec.rb
@@ -3,13 +3,18 @@ require 'rails_helper'
 RSpec.describe 'Restaurants', type: :request do
   describe 'GET /index' do
     before do
-      # FactoryBot.create_list(:restaurant, 10)
+      # @restaurants = FactoryBot.create_list(:restaurant, 10)
       @restaurants = FactoryBot.build_list(:restaurant, 10)
       get '/api/v1/restaurants'
     end
     
     it 'returns all restaurants' do
       expect(@restaurants.size).to eq(10)
+    end
+
+    it 'wont allow a restaurant without a name' do
+      restaurant = FactoryBot.build(:restaurant, name: nil)
+      expect(restaurant).to_not be_valid
     end
 
     it 'returns status code 200' do
